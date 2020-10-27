00feab46070ef84a8954c5192b70560a99a0ffa0
Lau2k1 <62877772+Lau2k1@users.noreply.github.com>
2020-10-27 17:35:53 +0300

    Refactoring code
    Refactoring code:
    Navbar.jsx
    Navbar.module.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import style from './Navbar.module.css';
 
 const Navbar = () => {
-    return <nav className={style.nav}>
-        <div className={style.item}>
-            <a href="/profile">Profile</a>
-        </div>
-        <div className={style.item}>
-            <a href="/dialogs">Messages</a>
-        </div>
-        <div className={style.item}>
-            <a href="/profile">News</a>
-        </div>
-        <div className={style.item}>
-            <a href="/profile">Music</a>
-        </div>
-        <div className={style.item}>
-            <a href="/profile">Settings</a>
-        </div>
-    </nav>
+    return (
+        <nav className={style.nav}>
+            <div className={style.item}>
+                <NavLink to="/profile" activeClassName={style.activeLink}>Profile</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to="/dialogs" activeClassName={style.activeLink}>Messages</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to="/news" activeClassName={style.activeLink}>News</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to="/music" activeClassName={style.activeLink}>Music</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink to="/setting" activeClassName={style.activeLink}>Settings</NavLink>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
