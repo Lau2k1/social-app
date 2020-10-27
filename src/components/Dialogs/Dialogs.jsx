1b6ceacac44b309610319df1db13e7ac7bbaded4
Lau2k1 <62877772+Lau2k1@users.noreply.github.com>
2020-10-27 18:37:19 +0300

    Refactoring code
    Refactoring cpde in:
    Dialogs.jsx
    Dialogs.module.css

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import style from './Dialogs.module.css'
+import style from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    Sanya Belikov
+                </div>
+                <div className={style.dialog}>
+                    Sanya Bratikov
+                </div>
+                <div className={style.dialog}>
+                    Daniil Degterev
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hi</div>
+                <div className={style.message}>How are you</div>
+                <div className={style.message}>Good, and you ?</div>
+                <div className={style.message}>Me to</div>
+            </div>
         </div>
     )
 }
 
-export default Dialogs
+export default Dialogs;
